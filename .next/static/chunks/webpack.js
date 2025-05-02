896687de7ed2689aa49cd9f3446aad68af0523e9
Emanuel Costa <82065728+emanuelcop3@users.noreply.github.com>
2025-05-02 10:53:15 -0300

    Update build artifacts and improve performance metrics
    - Updated various build artifacts in the `.next` directory, including cache and webpack files.
    - Modified webpack runtime hash for consistent builds.
    - Enhanced performance metrics in the trace file, reflecting improved request handling and resource loading times.
    - Adjusted CSS build processes for better optimization and memory usage tracking.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "615e310265d4e289"; }
+/******/ 		__webpack_require__.h = function() { return "147b2fd618afd494"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
